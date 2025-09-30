e8f821e915a7358a996e7bf8585d4ceb390bbffb
Nick Liu <imjustanemail@gmail.com>
2025-09-30 13:52:49 +0800

    fix(stream_scanner): 🐛 移除日志记录的负载数据
    * 删除对 `common.AppendPayloadChunkForLog` 的调用，以避免不必要的重复日志记录。
    * 改进数据处理逻辑，确保更高效的资源使用。

--- relay/helper/stream_scanner.go
+++ relay/helper/stream_scanner.go
@@ -244,7 +244,6 @@ func StreamScannerHandler(c *gin.Context, resp *http.Response, info *relaycommon
 			if !strings.HasPrefix(data, "[DONE]") {
 				info.SetFirstResponseTime()
 				info.ReceivedResponseCount++
-				common.AppendPayloadChunkForLog(c, constant.ContextKeyLoggedResponseBody, data)
 
 				select {
 				case dataChan <- data:
